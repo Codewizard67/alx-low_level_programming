7c7d39bd837682b5727fd4f8845c915febbb9ac5
codegeek <bkibish@gmail.com>
2023-07-20 15:05:15 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers -  prints the numbers, from 0 to 9
@@ -12,6 +13,7 @@ char c;
 for (c = '0'; c <= '9'; c++)
 {
 if (!(c == '2' || c == '4'))
-_putchar("c\n");
+putchar(c);
 }
+putchar('\n');
 }
